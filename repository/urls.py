7b99d3d18ce076ea31cbd65700f19dca00209b25
rjLelis <renatojlelis@gmail.com>
2019-11-07 15:52:23 -0300

    Add create repository api

--- repository/urls.py
+++ repository/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('repository/<str:repository_name>', views.get_repository, name='repository-list-by-name'),
-    path('repository/', views.RepositoryList.as_view(), name='repository-list')
+    #path('repository/<str:repository_name>', views.get_repository, name='repository-list-by-name'),
+    path('repository/', views.RepositoryListCreate.as_view(), name='repository-list'),
+    path('repository/<int:pk>', views.RepositoryDetail.as_view(), name='repository-detail'),
 ]
--- repository/views.py
+++ repository/views.py
@@ -3,30 +3,13 @@ from rest_framework.response import Response
 from rest_framework import generics
 from .models import Repository
 from .serializers import RepositorySerializer
-import requests
 
-@api_view(['GET'])
-def get_repository(request, repository_name):
-    
-    username = 'rjLelis'
-    repository_url = f'https://api.github.com/repos/{username}/{repository_name}'
-    api_response = requests.get(repository_url)
-    
-    if api_response.status_code != 200:
-        return Response({
-                'status': api_response.status_code, 
-                'message': api_response.json().get('message')
-            })
-    
-    new_repo = Repository.objects.create(
-                name=repository_name, 
-                description=api_response.json().get('description')
-            )
 
-    repo_serialized = RepositorySerializer(new_repo)
+class RepositoryListCreate(generics.ListCreateAPIView):
+    queryset = Repository.objects.all()
+    serializer_class = RepositorySerializer
 
-    return Response(repo_serialized.data)
 
-class RepositoryList(generics.ListAPIView):
+class RepositoryDetail(generics.RetrieveAPIView):
     queryset = Repository.objects.all()
     serializer_class = RepositorySerializer
