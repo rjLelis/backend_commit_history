f43b71548bfc08a0e713669bc7a9c6e4e665c40c
rjLelis <renatojlelis@gmail.com>
2019-11-14 16:12:37 -0300

    Change urls's names

--- repository/urls.py
+++ repository/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('repository/<str:repository_name>', views.get_repository, name='repository-list-by-name'),
-    path('repository/', views.RepositoryListCreate.as_view(), name='repository-list'),
-    path('repository/<int:pk>', views.RepositoryDetail.as_view(), name='repository-detail'),
+    path('/api', views.RepositoryListCreate.as_view(), name='repository-list'),
+    path('/api/<int:pk>', views.RepositoryDetail.as_view(), name='repository-detail'),
 ]
