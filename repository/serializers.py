0bdfe81c10f84bee1a8ed63587b4cc30066d8447
rjLelis <renatojlelis@gmail.com>
2019-11-20 11:54:35 -0300

    Add owner field in the serializer & Add User serializer

--- repository/serializers.py
+++ repository/serializers.py
@@ -1,8 +1,22 @@
-from .models import Repository
 from rest_framework import serializers
+from django.contrib.auth.models import User
+
+from .models import Repository
+
 
 class RepositorySerializer(serializers.ModelSerializer):
 
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Repository
-        fields = ('id', 'name', 'description', )
+        fields = ('id', 'name', 'description', 'owner', )
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    repositories = serializers.HyperlinkedRelatedField(
+        many=True, view_name='repository-detail', read_only=True)
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'repositories', )
