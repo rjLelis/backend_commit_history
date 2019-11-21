1af188ceab59537a5119756cde9831883ae20d9f
rjLelis <renatojlelis@gmail.com>
2019-11-21 16:41:51 -0300

    Add validation for owner on the RepositoryListCreate post request

--- repository/views.py
+++ repository/views.py
@@ -3,39 +3,58 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework import status
-from .models import Repository
+from .models import Repository, Owner
 from .serializers import RepositorySerializer
 
 
 class RepositoryListCreate(generics.ListCreateAPIView):
-    queryset = Repository.objects.order_by('created_at').all()
+    queryset = Repository.objects.order_by('-created_at').all()
     serializer_class = RepositorySerializer
 
     def post(self, request):
-        name = request.data.get('name')
-        description = request.data.get('description', '')
+        username = request.data.get('username')
+        repo_name = request.data.get('repo_name')
+        repo_description = request.data.get('repo_description', '')
 
-        if name is None or name.strip() == '':
+        if username.strip() is None or username.strip() == '':
+            return Response({
+                'message': 'the username of the repository most be filled'
+            },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        owner = Owner.objects.get(name=username)
+
+        if owner is None:
+            return Response({
+                'message': 'the user provided is not registered'
+            },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if repo_name.strip() is None or repo_name.strip() == '':
             return Response({
                 'message': 'the repository name is must be filled'
             },
                 status=status.HTTP_400_BAD_REQUEST
             )
 
-        _, created = Repository.objects.get_or_create(
-            name=name,
-            description=description
+        repo, created = Repository.objects.get_or_create(
+            name=repo_name,
+            description=repo_description,
+            owner=owner
         )
+
+        serializer = self.serializer_class(repo)
 
         if not created:
             return Response({
-                'message': 'the repository is already on the list'
+                'repository': serializer.data,
+                'message': 'the repository provided is already saved'
             },
-                status=status.HTTP_400_BAD_REQUEST
+                status=status.HTTP_409_CONFLICT
             )
 
-        repo.save()
-        serializer = self.serializer_class(repo)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
